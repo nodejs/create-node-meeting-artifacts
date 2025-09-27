50b9e0076d7c8a8efcb82d2d8ba9347ae88b2aef
avivkeller <me@aviv.sh>
2025-09-27 14:46:40 -0400

    use helper

--- src/github.mjs
+++ src/github.mjs
@@ -1,6 +1,6 @@
 import { Octokit } from '@octokit/rest';
 
-import { DEFAULT_CONFIG, REPOSITORY_URL_PREFIX_LENGTH } from './constants.mjs';
+import { DEFAULT_CONFIG } from './constants.mjs';
 
 /**
  * Creates a GitHub API client
@@ -43,11 +43,24 @@ export const createGitHubIssue = async (
 };
 
 /**
+ * Sorts issues by repository
+ * @param {Array<GitHubIssue>} issues The issues to sort
+ * @returns {Promise<{ [key: string]: Array<GitHubIssue> }>} Sorted issues
+ */
+export const sortIssuesByRepo = issues =>
+  issues.reduce((obj, issue) => {
+    (obj[issue.repository_url.split('/').slice(-2).join('/')] ||= []).push(
+      issue
+    );
+    return obj;
+  }, {});
+
+/**
  * Fetches GitHub issues from all repositories in an organization with a specific label
  * @param {import('@octokit/rest').Octokit} githubClient - Authenticated GitHub API client
  * @param {import('./types.d.ts').AppConfig} config - Application configuration
  * @param {import('./types.d.ts').MeetingConfig} meetingConfig - Meeting configuration
- * @returns {Promise<{ [key: string]: Array<GitHubIssue> }>} Formatted markdown string of issues
+ * @returns {Promise<{ [key: string]: Array<GitHubIssue> }>} Meeting agenda
  */
 export const getAgendaIssues = async (
   githubClient,
@@ -59,14 +72,9 @@ export const getAgendaIssues = async (
 
   // Get all issues/PRs in the organization
   const issues = await githubClient.paginate('GET /search/issues', {
-    q: `label:${agendaTag} org:${githubOrg}`,
+    q: `is:open label:${agendaTag} org:${githubOrg}`,
     advanced_search: true,
   });
 
-  return issues.reduce((obj, issue) => {
-    (obj[issue.repository_url.slice(REPOSITORY_URL_PREFIX_LENGTH)] ||= []).push(
-      issue
-    );
-    return obj;
-  }, {});
+  return sortIssuesByRepo(issues);
 };
